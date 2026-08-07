5238b6c669a5a81690bf3fe07c057cb6c10051b5
juniorboyd <watchara.sup@kkumail.com>
2026-08-07 11:21:41 +0700

    debug: move check-user script to src for docker compilation

--- backend/src/check-user.ts
+++ backend/src/check-user.ts
@@ -1,5 +1,5 @@
-import prisma from "./src/lib/prisma.ts";
-import { decrypt } from "./src/utils/crypto.ts";
+import prisma from "./lib/prisma.ts";
+import { decrypt } from "./utils/crypto.ts";
 import { generateSync } from "otplib";
 
 async function main() {
@@ -21,13 +21,16 @@ async function main() {
 
       const now = Math.floor(Date.now() / 1000);
       console.log("\nCodes relative to Server Time:");
+      console.log("- 2 min ago (T-120s) :", generateSync({ secret, epoch: now - 120 }));
+      console.log("- 1.5m ago (T-90s)   :", generateSync({ secret, epoch: now - 90 }));
       console.log("- 1 min ago (T-60s)  :", generateSync({ secret, epoch: now - 60 }));
       console.log("- 30s ago (T-30s)    :", generateSync({ secret, epoch: now - 30 }));
       console.log("- Current Time (now) :", generateSync({ secret, epoch: now }));
       console.log("- 30s later (T+30s)  :", generateSync({ secret, epoch: now + 30 }));
       console.log("- 1 min later (T+60s):", generateSync({ secret, epoch: now + 60 }));
+      console.log("- 1.5m later (T+90s) :", generateSync({ secret, epoch: now + 90 }));
       console.log("- 2 min later (T+120s):", generateSync({ secret, epoch: now + 120 }));
-    } catch (e) {
+    } catch (e: any) {
       console.error("Failed to decrypt secret:", e.message);
     }
   }
